38628b4f135fc500afa00a601694a8e15cdc2473
Egor Matisov <matisov.ea@phystech.edu>
2025-09-16 17:47:54 +0300

    Added digital-switch.py file with code of blinking led and button

---
+++ get-led/digital-switch.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+led = 26
+button = 13
+
+state = 0
+period = 1.0
+
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(button, GPIO.IN)
+
+while True:
+    if GPIO.input(button):
+        if state == 1:
+            state = 0
+        else:
+            state = 1
+        GPIO.output(led, state)
+        time.sleep(0.2)
