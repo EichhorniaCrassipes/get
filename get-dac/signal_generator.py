1f82c9f7377271b83144231d908476085afa87c1
Egor Matisov <matisov.ea@phystech.edu>
2025-10-07 18:33:13 +0300

    commit

--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -2,7 +2,14 @@ import numpy as np
 import time
 
 def get_sin_wave_amplitude(freq, time):
-    #some code here
+    signal = np.sin(2*np.pi*freq*t)
+    shifted = signal+1
+    normalized = shifted / 2.0
+    return normalized
 
 def wait_for_sampling_period(sampling_frequency):
-    #some code here
+    if sampling_frequency <= 0:
+        raise ValueError("Частота дискретизации должна быть положительной. Введите еще раз.\n")
+    
+    period = 1.0 / sampling_frequency
+    time.sleep(period)
