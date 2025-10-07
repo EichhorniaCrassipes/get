517033f9796ca311b16ec1b3e78404b2db406b44
Egor M <161571655+EichhorniaCrassipes@users.noreply.github.com>
2025-10-07 19:19:45 +0300

    Update pwm-sin.py

--- get-dac/pwm-sin.py
+++ get-dac/pwm-sin.py
@@ -1,8 +1,34 @@
-import pwm_dac as pw
+import pwm_dac as pwm
 import signal_generator as sg
 import time
 
-amplitude = 3.2          
-signal_frequency = 10   
-sampling_frequency = 1000  
+amplitude = 3.2         
+signal_frequency = 5    
+sampling_frequency = 1000 
 
+PWM_PIN = 12
+PWM_FREQUENCY = 10000 
+
+if __name__ == "__main__":
+    dac = None
+    try:
+        dac = pwm.PWM_DAC(
+            gpio_pin=PWM_PIN,
+            pwm_frequency=PWM_FREQUENCY,
+            dynamic_range=3.3,
+            verbose=False
+        )
+
+        start_time = time.time()
+
+        while True:
+            t = time.time() - start_time
+            normalized_amp = sg.get_sin_wave_amplitude(signal_frequency, t)
+            voltage = normalized_amp * amplitude
+
+            dac.set_voltage(voltage)
+            sg.wait_for_sampling_period(sampling_frequency)
+
+    finally:
+        if dac is not None:
+            dac.deinit()
