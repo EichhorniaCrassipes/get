bc71a41877b6ed48275123d0eb242e58e501f806
Egor Matisov <matisov.ea@phystech.edu>
2025-10-07 17:41:01 +0300

    edit mcp4725

--- get-dac/mcp4725_driver.py
+++ get-dac/mcp4725_driver.py
@@ -1,10 +1,10 @@
 import smbus
+import RPi.GPIO as GPIO
 
 class MCP4725:
     def __init__(self, dynamic_range, address=0x61, verbose=True):
         self.bus = smbus.SMBus(1)
-        print('hello world')
-        
+
         self.address = address
         self.wm = 0x00
         self.pds = 0x00
@@ -28,3 +28,25 @@ class MCP4725:
 
         if self.verbose:
             print(f"Число: {number}, отправленные по I2C данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}] \n")
+    def set_voltage(self, voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            if self.verbose:
+                print(f"Напряжение выходит за динамический диапазон ЦАП (0.0 – {self.dynamic_range:.3f} В)")
+                print("Устанавливаем 0.0 В")
+            voltage = 0.0
+try:
+    while True:
+        try:
+            voltage = float(input("Введите напряжение в Вольтах: "))
+            #number = voltage_to_number(voltage)
+            #number_to_dac(number)
+            #print(f"Число на вход в ЦАП: {number}")
+            #print(f"биты: {[int(element) for element in bin(number)[2:].zfill(8)]}")
+            
+        
+        except ValueError:
+            print("Вы ввели не число. Попробуйте еще раз\n")
+        
+finally:
+    #GPIO.output(dac_bits, 0)
+    GPIO.cleanup()
