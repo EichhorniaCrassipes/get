ac49ffcadf7ab149ea7f872b87b7bacac11d64c4
Egor M <161571655+EichhorniaCrassipes@users.noreply.github.com>
2025-10-07 19:32:59 +0300

    Update mcp4725-sin.py

--- get-dac/mcp4725-sin.py
+++ get-dac/mcp4725-sin.py
@@ -1,17 +1,21 @@
 import mcp4725_driver as mcp
 import signal_generator as sg
+import time
 
-amplitude = 3.2          
+amplitude = 3.2       
 signal_frequency = 10   
-sampling_frequency = 1000  
+sampling_frequency = 1000
+
+
+I2C_ADDRESS = 0x61
 
 if __name__ == "__main__":
     dac = None
     try:
-        mcp_obj = mcp.MCP4725(
-            gpio_pin=PWM_PIN,
-            pwm_frequency=PWM_FREQUENCY,
+
+        dac = mcp.MCP4725(
             dynamic_range=3.3,
+            address=I2C_ADDRESS,
             verbose=False
         )
 
@@ -20,9 +24,11 @@ if __name__ == "__main__":
         while True:
             t = time.time() - start_time
             normalized_amp = sg.get_sin_wave_amplitude(signal_frequency, t)
+ 
             voltage = normalized_amp * amplitude
 
             dac.set_voltage(voltage)
+
             sg.wait_for_sampling_period(sampling_frequency)
 
     finally:
