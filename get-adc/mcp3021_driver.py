0bd9afd0700468cf5dc840d4969aab42a43b533c
Egor Matisov <matisov.ea@phystech.edu>
2025-10-28 19:33:59 +0300

    cmt

---
+++ get-adc/mcp3021_driver.py
@@ -0,0 +1,11 @@
+import smbus
+
+class MCP3021:
+    def __init__(self, dynamic_range, verbose = False):
+        self.bus = smbus.SMBus(1)
+        self.dynamic_range = dynamic_range
+        self.address = 0x4D
+        self.verbose = verbose
+
+    def deinit(self):
+        self.bus.close()
